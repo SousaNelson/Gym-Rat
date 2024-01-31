bf91dccd4f38331884bb84e637661f587e8e2ee6
Fredamian <fredybrito03@gmail.com>
2024-01-31 16:39:54 -0100

    conecting database

--- app/src/main/java/cv/example/gymrat/MainActivity2.kt
+++ app/src/main/java/cv/example/gymrat/MainActivity2.kt
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.database.DatabaseReference
 
 class MainActivity2 : AppCompatActivity() {
-
 
     private lateinit var etNome : EditText
     private lateinit var etApelido : EditText
     private lateinit var btnEnter: Button
 
+    private lateinit var dbRed : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+
     }
 }
