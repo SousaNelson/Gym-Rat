f996ff41848d98209e9dd1e0ec7cb5907c84a5bd
Fredamian <fredybrito03@gmail.com>
2024-01-31 17:44:09 -0100

    google maps?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -48,6 +47,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
     implementation("com.google.firebase:firebase-analytics")
 }
