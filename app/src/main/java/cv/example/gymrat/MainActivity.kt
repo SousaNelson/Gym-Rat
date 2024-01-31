e7a208121f00139f273cfa879977527333f34052
Fredamian <fredybrito03@gmail.com>
2024-01-31 16:06:27 -0100

    version 2

--- app/src/main/java/cv/example/gymrat/MainActivity.kt
+++ app/src/main/java/cv/example/gymrat/MainActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-    lateinit var loading: Button
+    private lateinit var loading: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +18,8 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, MainActivity2::class.java)
             startActivity(intent)
         }
+
+
+
     }
 }
--- app/src/main/java/cv/example/gymrat/MainActivity2.kt
+++ app/src/main/java/cv/example/gymrat/MainActivity2.kt
@@ -2,8 +2,16 @@ package cv.example.gymrat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity2 : AppCompatActivity() {
+
+
+    private lateinit var etNome : EditText
+    private lateinit var etApelido : EditText
+    private lateinit var btnEnter: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
