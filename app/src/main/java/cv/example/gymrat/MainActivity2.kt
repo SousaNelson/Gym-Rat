1faef69986b3dd06e05d950dd0ccf7a3feaf59c9
Fredamian <fredybrito03@gmail.com>
2024-01-31 17:06:37 -0100

    database conected

--- app/src/main/java/cv/example/gymrat/MainActivity2.kt
+++ app/src/main/java/cv/example/gymrat/MainActivity2.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -12,11 +13,16 @@ class MainActivity2 : AppCompatActivity() {
     private lateinit var etApelido : EditText
     private lateinit var btnEnter: Button
 
-    private lateinit var dbRed : DatabaseReference
+    private lateinit var dbRef : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        etNome = findViewById(R.id.etNome)
+        etNome = findViewById(R.id.etApelido)
+        btnEnter = findViewById(R.id.btnEnter)
+
+        dbRef = FirebaseDatabase.getInstance().getReference("Rats")
 
     }
 }
