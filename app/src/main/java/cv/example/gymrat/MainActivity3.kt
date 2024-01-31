218f51450e3277688bf23ea78f0c727e26ebd8a6
SousaNelson <nsousa@uta.cv>
2024-01-31 16:14:44 +0000

    version 3

--- app/src/main/java/cv/example/gymrat/MainActivity3.kt
+++ app/src/main/java/cv/example/gymrat/MainActivity3.kt
@@ -3,9 +3,23 @@ package cv.example.gymrat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity3 : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity3 : AppCompatActivity() {}
+    //private lateinit var binding: ActivityMainBinding
+   /* override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+
+
+        adapter.addFragment(BasicoFragment(), "Train")
+        adapter.addFragment(DiarioFragment(),"Track" )
+        adapter.addFragment(OutroFragment(), "")
+
+        binding.viewPager.adapter = adapter
+        binding.tab.setupWithViewPager(binding.viewPager)
     }
 }
+
+*/
