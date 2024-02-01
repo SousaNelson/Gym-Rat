e567ef67e0cbffcc2ea382883dad0eb68dfd4a4c
Fredamian <fredybrito03@gmail.com>
2024-01-31 23:34:55 -0100

    changes

--- app/src/main/java/cv/example/gymrat/TrackFragment.kt
+++ app/src/main/java/cv/example/gymrat/TrackFragment.kt
@@ -1,16 +1,53 @@
-import android.annotation.SuppressLint
+package cv.example.gymrat
+
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import cv.example.gymrat.R
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
-
-class MainActivity : AppCompatActivity() {
-
-
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TrainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TrackFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_train, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment TrainFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            TrainFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
 }
